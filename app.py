97f5fd4326895fc858337023f9e41732c5b76aa2
S1161110 <s1161110@gm.ncue.edu.tw>
2025-10-22 03:32:03 +0000

    更新首頁

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import streamlit as st
-#主頁面
-st.title("王筱涵")
-st.header("王筱涵的GIS專案")
-st.write("資管系大四S1161110，本網頁的誕生源於交功課，以及祈禱S15的CFO能取得佳績")
 
 # 1. 使用 st.Page() 定義頁面
 # st.Page() 會自動尋找相符.py檔案
 pages = [
-#    st.Page("page_home.py", title="專案首頁", icon="👻"),
-    st.Page("map_viewer.py", title="互動地圖瀏覽", icon="🫤"),
+    st.Page("home.py", title="專案首頁", icon="👻"),
+#    st.Page("map_viewer.py", title="互動地圖瀏覽", icon="🫤"),
 #    st.Page("page_about.py", title="關於我們", icon="🫵")
 ]
 # 2. 使用 st.navigation() 建立導覽
---
+++ home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+#主頁面
+st.title("王筱涵")
+st.header("王筱涵的GIS專案")
+st.write("資管系大四S1161110，本網頁的誕生源於交功課，以及祈禱S15的CFO能取得佳績")
